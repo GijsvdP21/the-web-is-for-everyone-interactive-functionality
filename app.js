5a61ba7cb33ff794c7d35fa64b294515be8c87d6
GijsvdP21 <gijsvandeputte@icloud.com>
2023-04-10 16:56:51 +0100

    update

--- app.js
+++ app.js
@@ -1,93 +1,183 @@
-import express from 'express'
-import bodyParser from 'body-parser'
-import fs from 'fs'
-import path from 'path'
-import { get } from 'http'
+import express from "express";
+import bodyParser from "body-parser";
+import fs from "fs";
+import path from "path";
+import { get } from "http";
 
 // Create a new express app
-const app = express()
+const app = express();
 
 // Load json files
-const gameData = fs.readFileSync('./public/api/game/943.json')
-
+const gameData = fs.readFileSync("./public/api/game/943.json");
 
 // Parse data to json object
-const parsedData = JSON.parse(gameData)
+const parsedData = JSON.parse(gameData);
 
-console.log(parsedData)
+console.log(parsedData);
 
 // Every JSON file in the player folder
-const playerExtraData = fs.readdirSync('./public/api/facts/Player').filter(file => path.extname(file) === '.json');
-let extraPlayerData= []
+const playerExtraData = fs
+  .readdirSync("./public/api/facts/Player")
+  .filter((file) => path.extname(file) === ".json");
+let extraPlayerData = [];
 // console.log(playerExtraData)
 
 // Read every file and parse it to json
-playerExtraData.forEach(file => {
-    const fileData = fs.readFileSync(path.join('./public/api/facts/Player/', file))
-    let parsedFileData = JSON.parse(fileData)
-    // console.log(parsedFileData)
+playerExtraData.forEach((file) => {
+  const fileData = fs.readFileSync(
+    path.join("./public/api/facts/Player/", file)
+  );
+  let parsedFileData = JSON.parse(fileData);
+  // console.log(parsedFileData)
 
-    // Get the id of the player based on the json file name
-    let playerExtraId = file.replace('.json', '')
+  // Get the id of the player based on the json file name
+  let playerExtraId = file.replace(".json", "");
 
-    // Convert the id string to an integer
-    playerExtraId = parseInt(playerExtraId)
-    
-    // Add the id to the json object
-    parsedFileData.id = playerExtraId
+  // Convert the id string to an integer
+  playerExtraId = parseInt(playerExtraId);
 
-    // Add the json object to the data array
-    extraPlayerData = [...extraPlayerData, parsedFileData]
-})
+  // Add the id to the json object
+  parsedFileData.id = playerExtraId;
+
+  // Add the json object to the data array
+  extraPlayerData = [...extraPlayerData, parsedFileData];
+});
 
 // console.log(extraPlayerData)
 
-
 // Set the view engine of the app to ejs
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 // Set the location of the views folder
-app.set('views', './views')
+app.set("views", "./views");
 // Set the location of the public folder with static files
-app.use(express.static('public'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/', async (req, res) => {
-    const data = parsedData
+app.get("/", async (req, res) => {
+  const data = parsedData;
 
-    if (data.hasStatistics === true) {
-        let gameStats = fs.readFileSync(`./public/api/game/${data.gameId}/statistics.json`)
-        let parsedStats = JSON.parse(gameStats)
+  if (data.hasStatistics === true) {
+    let gameStats = fs.readFileSync(
+      `./public/api/game/${data.gameId}/statistics.json`
+    );
+    let parsedStats = JSON.parse(gameStats);
 
-        data.team1CountryISO2Code = data.team1CountryISO2Code.toLowerCase()
-        data.team2CountryISO2Code = data.team2CountryISO2Code.toLowerCase()
+    data.team1CountryISO2Code = data.team1CountryISO2Code.toLowerCase();
+    data.team2CountryISO2Code = data.team2CountryISO2Code.toLowerCase();
 
-        res.render('index', {data, parsedStats, extraPlayerData})
+    res.render("index", { data, parsedStats, extraPlayerData, active: "/" });
+  } else {
+    res.render("index", { data, active: "/" });
+  }
+});
+
+app.post("/new", (request, response) => {
+  console.log(request.body);
+  postJson(`${url}/urls`, request.body).then((data) => {
+    let newURL = { ...request.body };
+
+    if (data.success) {
+      response.redirect("/?urlPosted=true");
     } else {
-        res.render('index', {data})
-    }    
-})
+      const errorMessage = data.message;
+      const newData = { error: errorMessage, values: newURL };
 
-app.post('/new', (request, response) => {
-    console.log(request.body)
-    postJson(`${url}/urls`, request.body).then((data) => {
-      let newURL = { ...request.body }
-  
-      if (data.success) {
-        response.redirect('/?urlPosted=true')
-      } else {
-        const errorMessage = data.message
-        const newData = { error: errorMessage, values: newURL }
-  
-        fetchJson(`${url}/websites`).then((data) => {
-          response.render('projects', { data: data, active: '/projects' })
-        })
-      }
-    })
-})
+      fetchJson(`${url}/websites`).then((data) => {
+        response.render("projects", { data: data, active: "/projects" });
+      });
+    }
+  });
+});
 
-app.set('port', process.env.PORT || 8000)
+app.set("port", process.env.PORT || 8000);
 
-app.listen(app.get('port'), function () {
-    console.log(`Application started on http://localhost:${app.get('port')}`)
-})
+app.listen(app.get("port"), function () {
+  console.log(`Application started on http://localhost:${app.get("port")}`);
+});
+
+// Route voor de spelers
+app.get("/players", async (req, res) => {
+  const data = parsedData;
+
+  if (data.hasStatistics === true) {
+    let gameStats = fs.readFileSync(
+      `./public/api/game/${data.gameId}/statistics.json`
+    );
+    let parsedStats = JSON.parse(gameStats);
+
+    data.team1CountryISO2Code = data.team1CountryISO2Code.toLowerCase();
+    data.team2CountryISO2Code = data.team2CountryISO2Code.toLowerCase();
+
+    res.render("players", {
+      data,
+      parsedStats,
+      extraPlayerData,
+      active: "/players",
+    });
+  } else {
+    res.render("players", { data, active: "/players" });
+  }
+});
+
+// Route voor de nieuwe stats
+app.get("/addStats", async (req, res) => {
+  const data = parsedData;
+
+  if (data.hasStatistics === true) {
+    let gameStats = fs.readFileSync(
+      `./public/api/game/${data.gameId}/statistics.json`
+    );
+    let parsedStats = JSON.parse(gameStats);
+
+    data.team1CountryISO2Code = data.team1CountryISO2Code.toLowerCase();
+    data.team2CountryISO2Code = data.team2CountryISO2Code.toLowerCase();
+
+    res.render("addStats", {
+      data,
+      parsedStats,
+      extraPlayerData,
+      active: "/addStats",
+    });
+  } else {
+    res.render("addStats", { data, active: "/addStats" });
+  }
+});
+
+// Route voor de score
+app.get("/score", async (req, res) => {
+  const data = parsedData;
+
+  if (data.hasStatistics === true) {
+    let gameStats = fs.readFileSync(
+      `./public/api/game/${data.gameId}/statistics.json`
+    );
+    let parsedStats = JSON.parse(gameStats);
+
+    data.team1CountryISO2Code = data.team1CountryISO2Code.toLowerCase();
+    data.team2CountryISO2Code = data.team2CountryISO2Code.toLowerCase();
+
+    res.render("score", {
+      data,
+      parsedStats,
+      extraPlayerData,
+      active: "/score",
+    });
+  } else {
+    res.render("score", { data, active: "/score" });
+  }
+});
+
+const apiUrl = "https://api.ultitv.fdnd.nl/api/v1/questions";
+
+app.get("/", (request, response) => {
+  fetchJson(apiUrl).then((data) => {
+    response.render("index", data);
+  });
+});
+
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
--- public/script/script.js
+++ public/script/script.js
@@ -19,3 +19,54 @@ personas.forEach((persoon)=>{
 
     })
 })
+
+
+function AddGoal() {
+    // Get the checkbox
+    const checkBoxGoal = document.querySelector(".checkboxGoal");
+    // Get the output text
+    const goals = document.querySelector(".goalMore");
+  console.log(goals)
+    // If the checkbox is checked, display the output text
+    goals.classList.toggle('pen')
+}
+
+function AddAssist() {
+    // Get the checkbox
+    const checkBoxAssist = document.querySelector(".checkboxAssist");
+    // Get the output text
+    const Assists = document.querySelector(".assistMore");
+  console.log(Assists)
+    // If the checkbox is checked, display the output text
+    Assists.classList.toggle('pen')
+}
+
+function AddBlock() {
+    // Get the checkbox
+    const checkBoxBlock = document.querySelector(".checkboxBlock");
+    // Get the output text
+    const Blocks = document.querySelector(".blockMore");
+  console.log(Blocks)
+    // If the checkbox is checked, display the output text
+    Blocks.classList.toggle('pen')
+}
+
+function AddTurnover() {
+    // Get the checkbox
+    const checkBoxTurnover = document.querySelector(".checkboxTurnover");
+    // Get the output text
+    const Turnovers = document.querySelector(".turnoverMore");
+  console.log(Turnovers)
+    // If the checkbox is checked, display the output text
+    Turnovers.classList.toggle('pen')
+}
+
+function AddPass() {
+    // Get the checkbox
+    const checkBoxPass = document.querySelector(".checkboxPass");
+    // Get the output text
+    const Pass = document.querySelector(".passMore");
+  console.log(Pass)
+    // If the checkbox is checked, display the output text
+    Pass.classList.toggle('pen')
+}
